6a173ee37e9cd3d41c7dc6c4bb64c99f3e21cf8f
Phil <philphoo95@gmail.com>
2015-08-01 13:34:52 -0400

    Working add to SQL table

--- addpost.php
+++ addpost.php
@@ -1,8 +1,23 @@
-<html>
-	<body>
-		<?php
-			echo "Hi, this file is here";
-			echo $_POST['title'];
-		?>
-	</body>
-</html>
+<?php
+	//Get number of posts in database
+	$host = "localhost";
+	$username = "root";
+	$password = "";
+	$db_name = "LoginSystem";
+	$tbl_name = "posts";
+
+	mysql_connect($host, $username, $password) or die("Cannot Connect!");
+	mysql_select_db($db_name) or die("Cannot select Database");
+
+	$sql = "SELECT *FROM $tbl_name";
+
+	//Creating post ID - simply the number of rows
+	$id = mysql_num_rows(mysql_query($sql));
+
+	//Get title and post from AJAX
+	$title = $_POST['title'];
+	$content = $_POST['postcontent'];
+
+	$addPostQuery = "INSERT INTO $tbl_name(postID, title, content) VALUES('$id', '$title', '$content')";
+	mysql_query($addPostQuery);
+?>
